7a74a5d3385dbafbcb76f7e954ace6947d481b74
Emily Safron <36644265+ejsafron@users.noreply.github.com>
2020-11-04 15:56:08 -0600

    Update build-metafeatures.py

--- aux/build-metafeatures.py
+++ aux/build-metafeatures.py
@@ -8,7 +8,7 @@ Created on Thu Oct  1 18:25:40 2020
 
 For the purpose of filtering out signals from known short-period objects and still being left with those from potentially unknown long-period objects, we must score by the transit feature, rather than by the light curve.  Though this is computationally intensive, it will give us a very straightforward way to filter our results, in the end.
 
-To score these features, we first need an iterable list of them.  For this, we go back to the user marks themselves.  We go through all the global xmin and xmax ranges, mark by mark, and quantify by measure of overlap fraction and closeness of marking midpoint, which chunks of x-range indicate markings which are "alike" enough to be consolidated together.  The consolidated markings, which we call metafeatures, are written and saved to a csv.
+To score these features, we first need an iterable list of them.  For this, we go back to the user marks themselves.  We go through all the global xmin and xmax ranges, mark by mark, and quantify by measure of closeness of marking midpoint, which chunks of x-range indicate markings which are "alike" enough to be consolidated together (requiring that matching marks have non-zero overlap).  The consolidated markings, which we call metafeatures, are written and saved to a csv.
 
 This code takes as input:
     mdwarf-classifs.csv
@@ -17,20 +17,9 @@ and produces as output:
     metafeatures.csv.
 
 """
-# combining mfs w/ >75% overlap
-# 717273 metafeatures
-# 31148 seconds
-
-# overlap req. = 0.75
-# 942831 mfs
-# 19830 sec
-
-# overlap req. = 0.6, combining mfs w/ >75% overlap
-# 675831 mfs
-# 33344 sec
 
 
-# This edited version of the metafeature list builder DOES allow a single user mark to contribute to multiple metafeatures.
+# This edited version of the metafeature list builder DOES NOT allow a single user mark to contribute to multiple metafeatures.
 
 
 import numpy as np
@@ -48,9 +37,8 @@ start_time = timeit.default_timer()
 
 
 ''' Changeable pipeline parameters '''
-tolerance = 1.0     # How close (in days) the midpoints of two user markings must be if indicating the same feature
-single = 2.0        # Maximum duration (in days) of consolidatable marks; marks wider than this will automatically be their own metafeatures
-cutoff = 6.0        # Duration (in days) of the longest user marking we're willing to assume was made intentionally
+tolerance = 0.8      # How close (in days) the midpoints of two user markings must be if indicating the same feature
+cutoff = 3.0         # Duration (in days) of the longest user marking we're willing to assume was made intentionally
 
 
 
@@ -60,9 +48,6 @@ cutoff = 6.0        # Duration (in days) of the longest user marking we're willi
 db = pd.read_csv('/home/esafron/Documents/PH/mdwarf-classifs.csv')      # masotan
 
 
-# FOR TESTING:  ONLY KOI TARGETS
-#kt = pd.read_csv('/home/esafron/Documents/PH/knowntransits-nea.csv')
-
 print('Completed setting up list of data arrays.')
 
 
@@ -70,9 +55,6 @@ print('Completed setting up list of data arrays.')
 ''' Set up the transit score table '''
 # Get list of user transit markings for each non-synthetic light curve
 setsubjectids = list(set(db['subject_id']))  # Set of all unique lightcurve ids INCLUDING SYNTHETICS
-
-# FOR TESTING:
-#setsubjectids = list(set(kt['curveid']))
 
 
 transitids,lightcurves,kicids,xmins,xmaxs,durations,midpoints,synth_flag,dupe_flag = [],[],[],[],[],[],[],[],[]
@@ -128,7 +110,7 @@ for i in range(len(setsubjectids)):
             numdata = sum(not np.isnan(x) for x in time if xmin <= x <= xmax)   # Count real data in range of mark duration
             datafrac = float(numdata)/float(numpoints)                          # Calculate real data fraction
             
-            matchidx = [j]                                                          
+            matchidx = [j]                                                      # List of matches includes original mark by default    
                     
             if duration >= cutoff:                                              # Filter out marks wider than cutoff
                 continue
@@ -136,8 +118,6 @@ for i in range(len(setsubjectids)):
                 continue
             elif datafrac < 0.5:
                 continue                                                        # Filter out marks for which > 50% lie in data gaps
-            elif duration > single:
-                pass                                                            # Don't consolidate if duration > consolidation cutoff
             else:
                 for k in range(len(subidclasses)):
                     testxmin = subidclasses['xMinGlobal'].iloc[k]
@@ -153,7 +133,7 @@ for i in range(len(setsubjectids)):
                         continue
                     elif k in done:                                             # Don't compare a mark to another mark that's already been used
                         continue
-                    elif testduration > single:                                 # Ignore very wide marks
+                    elif testduration >= cutoff:                                # Ignore very wide marks
                         continue
                     elif testdatafrac < 0.5:                                    # Don't look at marks for which > 50% lie in data gaps
                         continue
@@ -171,7 +151,7 @@ for i in range(len(setsubjectids)):
                 if len(matchidx)==1:                                            # If the original index is the only one in the match list
                     pass                                                        # Skip right to the metafeature tabulation
                 else:
-                    matchidx_new = list(matchidx)                               # Indices of bad usermarks will be popped from this copy
+                    matchidx_list = [matchidx]                                  # Initialize list of match lists, hold on tight
                     for y in range(len(matchidx)-1):
                         # Make y the base mark, rerun test
                         idx = matchidx[y+1]     # skip the original mark
@@ -194,29 +174,35 @@ for i in range(len(setsubjectids)):
                             testnumpoints_y = testduration_y / delta
                             testnumdata_y = sum(not np.isnan(x) for x in time if testxmin_y <= x <= testxmax_y)
                             testdatafrac_y = float(testnumdata_y)/float(testnumpoints_y)
-            
-                            # Same tests as before, except we DO want to allow it to check marks that are already "done"
                             
                             if yk == idx:                                       # Don't compare a mark to itself
+                                continue
+                            elif yk in done:                                    # Don't compare to marks already consolidated
                                 continue
                             elif testduration_y >= cutoff:                      # Continue to ignore very wide marks
                                 continue
                             elif testdatafrac_y < 0.5:                          # Don't look at marks for which > 50% lie in data gaps
                                 continue
                             else:
-                                testmark_y = [testxmin_y,testxmax_y]
                                 if np.abs(midpoint_y-testmidpoint_y) > tolerance:   # If matching criteria is not met
                                     continue                                        # Move to next test index
                                 else:                                               # If matching criteria is met
                                     matchidx_y.append(yk)                           # Add subidclass index to matchlist
                                     
-                        # Match lists will not be in the same order
-                        if set(matchidx) == set(matchidx_y):                                # If matchlists are the same
-                            continue                                                        # Leave that usermark in the original match list
-                        elif set(matchidx) != set(matchidx_y):                              # If any usermarks are added or missing
-                            popidx = int(np.where(np.asarray(matchidx_new)==idx)[0][0])     # Find index of bad mark in match list copy
-                            matchidx_new.pop(popidx)                                        # Remove that index
+                        matchidx_list.append(matchidx_y)             # Append match list to list of lists
+                        
+                    # When done with all y indices, we should have a list of "sets,"
+                    # some of which may match the original set and some of which may not
                     
+                    matchidx_new = [j]                                          # Make new list to store indices of identical lists
+                    for jy in range(len(matchidx)-1):
+                        jset = set(matchidx)
+                        yset = set(matchidx_list[jy+1])
+                        if jset==yset:                                          # If the match set for two indices is identical
+                            matchidx_new.append(matchidx[jy+1])                 # Add index corresponding to set to the list (NOT the whole set)
+                        elif jset!=yset:                                        # Otherwise,
+                            continue                                            # Continue
+                                            
                     matchidx = list(matchidx_new)                               # Redefine the match list
                     
             matchlist_xmin, matchlist_xmax = [], []
@@ -250,9 +236,10 @@ for i in range(len(setsubjectids)):
 # Build dataframe
 metafeatures = pd.DataFrame(np.column_stack([transitids, lightcurves, kicids, xmins, xmaxs, durations, midpoints, synth_flag, dupe_flag]), columns=['transitids', 'lightcurves', 'kicids', 'xmin', 'xmax', 'durations', 'midpoints', 'synth_flag', 'dupe_flag'])
 
+
 # Export to csv
 metafeatures.to_csv(user_directory+'metafeatures_new.csv', index=False)
-
+metafeatures.to_csv(user_directory+'metafeatures-tol'+str(tolerance)[0]+str(tolerance)[2]+'-cut'+str(cutoff)[0]+str(cutoff)[2]+'.csv', index=False)
 
 print('Transits for scoring isolated.')
 elapsed = timeit.default_timer() - start_time
