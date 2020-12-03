45991fe577ee3fbf7d1e7faaeac1f1d6677d875f
Emily Safron <36644265+ejsafron@users.noreply.github.com>
2020-12-03 14:22:51 -0600

    Update build-uwtable.py
    - Removed PH-specific dependencies
    - Made consistent with new fieldnames of metafeatures.csv

--- main/build-uwtable.py
+++ main/build-uwtable.py
@@ -6,10 +6,10 @@ Python ver. 3.6.0
 
 @author: Emily Safron
 
-This code should run after building the transit list and the userweight seeds, but before the coupled iteration of transit scores and userweights.  It creates the userweight table for iteration.  In addition, this code refines the userweight table to include, for each user, a list of:
-        - which metafeature/transit ids they have classified,
-        - how many total metafeatures + synthetic transits they have classified,
-        - and how many total lightcurves they have classified,
+This code should run after building the metafeature list and the userweight seeds, but before the coupled iteration of scores and userweights.  It creates the userweight table for iteration.  In addition, this code refines the userweight table to include, for each user, a list of:
+        - which metafeature ids they have classified,
+        - how many total metafeatures they have classified,
+        - and how many total subjects they have classified,
 for ease of reference in the iteration code.
 
 This code requires the following files as input:
@@ -31,10 +31,9 @@ import multiprocessing as mp
 
 
 # For easy replacement
-user_directory = '/home/safron/Documents/PH/master/'    # sif
+user_directory = '/home/safron/Documents/PH/master/11242020/'    # sif
 
-# Adjustable dummy weight to be assigned to any user who has not seen simulations
-dummy = 1.
+
 
 start_time = timeit.default_timer()
 
@@ -52,19 +51,19 @@ uw_seeds = pd.read_csv(user_directory+'user-weighting.csv',names=columns)
 
 # The metafeatures
 metafeatures = pd.read_csv(user_directory+'metafeatures.csv')
-#  ||  transitids  |  lightcurves |  kicids  |  xmin  |  xmax  |  durations  |  midpoints  |  synth_flag  |  dupe_flag  ||
-metafeatures = metafeatures[np.isnan(metafeatures['midpoints'])==False]         # Remove dupes
+#  ||  feature_id  |  subject_id  |  kicids  |  xmin  |  xmax  |  durations  |  midpoints  |  synth_flag  ||
 
 
 
 ''' Set up the userweight table for iteration '''
 # Setting up the user list and initializing the user weights
 # The only things we'll need from the seed table are username, normcombined, and maybe numclasses
-# But we need to distinguish the number of lightcurve chunk classifications from the number of transit classifications, which we also need
+# But we need to distinguish the number of subject classifications from the number of metafeature classifications, which we also need
 allusers = list(set(db['user_name']))
-userweights = Table({'username':allusers,'normcombined':list(dummy for i in range(len(allusers))),'numLCclasses':np.zeros(len(allusers)),'numtransitclasses':np.zeros(len(allusers)),'transitsclassified':np.zeros(len(allusers))}, names=['username','normcombined','numLCclasses','numtransitclasses','transitsclassified'])        # 0.787419222 for mean
+userweights = Table({'username':allusers,'normcombined':list(1. for i in range(len(allusers))),'numsubjectclasses':np.zeros(len(allusers)),'numfeatureclasses':np.zeros(len(allusers)),'featuresclassified':np.zeros(len(allusers))}, names=['username','normcombined','numsubjectclasses','numfeatureclasses','featuresclassified'])
 
 userweights = userweights.to_pandas()
+
 
 print("Completed setting up list of data arrays.")
 
@@ -81,34 +80,30 @@ for i in range(len(uw_seeds['username'])):
             
 print('Userweight seeds copied.')
 
-# Must make transitsclassified column dtype object so that we can insert lists there
-userweights = userweights.astype({'transitsclassified': object})
+# Must make featuresclassified column dtype object so that we can insert lists there
+userweights = userweights.astype({'featuresclassified': object})
 
 
 
-
-
-
-''' Now have to  construct 'transitsclassified' column for the userweights table, from users who classified each transit_id and stick that in the dataframe, and count/add them to the synthetics to obtain 'numtransitclasses', also for the userweights table.  '''
 
 
 print("Counting and recording metafeatures classified by users..")
 
 
-''' Build the list of transit IDs classified by simply combining the list of lightcurves from the classifications db with the unscored metafeatures db.  This produces a list containing duplicates but the dupes are easy to weed out using set(). '''
+''' Build the list of metafeature IDs classified by simply combining the list of subjects from the classifications db with the unscored metafeatures db. '''
 
 
 # A function to find the transit IDs classified by a user
 def which_features_classified(the_user):
-    user_transits = []                                                                          # Place to stick the transit IDs as we find them
-    curveids = list(set(db['subject_id'][db['user_name']==the_user]))                           # All lightcurves classified by the user in db
-    for i in range(len(curveids)):
-        transits_in_curve = list(metafeatures['transitids'][metafeatures['lightcurves']==curveids[i]])  # List of all transit IDs corresponding to lightcurve
+    user_features = []                                                                          # Place to stick the transit IDs as we find them
+    subjectids = list(set(db['subject_id'][db['user_name']==the_user]))                         # All lightcurves classified by the user in db
+    for i in range(len(subjectids)):
+        features_in_subject = list(metafeatures['feature_id'][metafeatures['subject_id']==subjectids[i]])  # List of all transit IDs corresponding to lightcurve
                                                                                                 # in metafeature list
-        for j in range(len(transits_in_curve)):
-            user_transits.append(transits_in_curve[j])                                          # Append each transit ID to the list
-    user_transits = list(set(user_transits))                                                    # Shouldn't be any duplicates, but remove if so
-    return [the_user, user_transits, len(curveids)]
+        for j in range(len(features_in_subject)):
+            user_features.append(features_in_subject[j])                                        # Append each transit ID to the list
+    user_features = list(set(user_features))                                                    # Shouldn't be any duplicates, but remove if so
+    return [the_user, user_features, len(subjectids)]
 
 
 # Initialize timer
@@ -117,10 +112,10 @@ start_time = timeit.default_timer()
 
 # Initialize pool
 pool = mp.Pool(mp.cpu_count())
-which_trans_results = []
+which_feature_results = []
 
 # Asynchronous mapping, for ALL users
-which_trans_results = pool.map_async(which_features_classified, [user for user in allusers]).get()
+which_feature_results = pool.map_async(which_features_classified, [user for user in allusers]).get()
 
 # Close pool
 pool.close()
@@ -130,14 +125,14 @@ pool.close()
 user_check = []
 featuresclassifiedlist = []
 featurecountlist = []
-curvecountlist = []
+subjectcountlist = []
 for i in range(len(userweights['username'])):                                   # i is user index in userweights table (same length as allusers list)
     user = userweights['username'].iloc[i]                                      # username string
-    idx = np.where(np.asarray(which_trans_results)[:,0]==user)[0][0]            # index location in user_check results list
-    user_check.append(which_trans_results[idx][0])                              # username from results list
-    featuresclassifiedlist.append(which_trans_results[idx][1])                  # list of metafeatures classified
-    featurecountlist.append(int(len(which_trans_results[idx][1])))              # number of metafeatures classifed
-    curvecountlist.append(which_trans_results[idx][2])                          # number of lightcurves classifed
+    idx = np.where(np.asarray(which_feature_results)[:,0]==user)[0][0]          # index location in user_check results list
+    user_check.append(which_feature_results[idx][0])                            # username from results list
+    featuresclassifiedlist.append(which_feature_results[idx][1])                # list of metafeatures classified
+    featurecountlist.append(int(len(which_feature_results[idx][1])))            # number of metafeatures classifed
+    subjectcountlist.append(which_feature_results[idx][2])                      # number of lightcurves classifed
 
 
 
@@ -151,10 +146,10 @@ print('Run time = '+str(elapsed)+' sec.')
 ''' We finally have all the lists we need! '''
 
 # Fold into full userweight seed dataframe
-full_uwseeds = pd.DataFrame(np.column_stack([allusers, user_check, list(userweights['normcombined']), curvecountlist, featurecountlist, featuresclassifiedlist]), columns=['username','user_check','normcombined', 'numLCclasses', 'numtransitclasses', 'transitsclassified'])
+full_uwseeds = pd.DataFrame(np.column_stack([allusers, user_check, list(userweights['normcombined']), subjectcountlist, featurecountlist, featuresclassifiedlist]), columns=['username','user_check','normcombined', 'numsubjectclasses', 'numfeatureclasses', 'featuresclassified'])
 
-# Make numLCclasses column into integers
-full_uwseeds['numLCclasses'] = full_uwseeds.numLCclasses.astype(int)
+# Make numsubjectclasses column into integers
+full_uwseeds['numsubjectclasses'] = full_uwseeds.numsubjectclasses.astype(int)
 
 # Save as .tsv file
 full_uwseeds.to_csv(user_directory+'full_uwseeds.tsv', sep="\t", index=False)
